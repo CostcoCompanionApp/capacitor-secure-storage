bae26108d7f92064387f239ed6e328f08afc1a18
Aleksandr Melentiev <zapper@gmail.com>
2025-05-04 12:28:18 -0700

    chore(build): add prepare script command and untrack dist

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { SecureStoragePlugin } from './definitions';
-declare const proxy: SecureStoragePlugin;
-export * from './definitions';
-export { proxy as SecureStorage };
--- dist/esm/index.js
+++
@@ -1,8 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const proxy = registerPlugin('SecureStorage', {
-    web: async () => import('./web').then((module) => new module.SecureStorageWeb()),
-    ios: async () => import('./native').then((module) => new module.SecureStorageNative(proxy)),
-    android: async () => import('./native').then((module) => new module.SecureStorageNative(proxy)),
-});
-export * from './definitions';
-export { proxy as SecureStorage };
