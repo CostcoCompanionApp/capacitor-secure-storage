0b2868edc48e62e7794ff4a07b901effac1bf918
Aleksandr Melentiev <zapper@gmail.com>
2025-05-04 12:48:42 -0700

    chore(ios): Capacitor v7: update podspec

--- simple-git-hooks.js
+++ simple-git-hooks.js
@@ -4,6 +4,5 @@
   Be sure to run `simple-git-hooks` after modifying this file.
 */
 module.exports = {
-  'commit-msg': 'cat "$@" | "$PWD"/node_modules/.bin/commitlint',
   'pre-commit': 'pnpm lint',
 }
